d56adb714bd7dfda8ea16ac309fd61b1c0a1b7c2
nezen <nezen@USERPC>
2025-07-14 12:01:58 +0300

    Add project files.

---
+++ Csharp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tHello, World!");
+            Console.WriteLine("I am learning C#");
+        }
+    }
+}
